2a55927331319c399c60512128e7340fa5f93fff
Mateusz Duda <the_pro@wp.pl>
2020-10-07 22:39:50 +0200

    Corrected yadm

--- remote-install.sh
+++ remote-install.sh
@@ -3,9 +3,9 @@
 # check if yadm is installed
 if ! command -v COMMAND &> /dev/null
 then
-    curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
+    curl -fLo .yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x .yadm
     exit
 fi
 
-yadm clone https://github.com/c2v4/dotfiles
-yadm submodule update --init --recursive`
+./.yadm clone https://github.com/c2v4/dotfiles
+./.yadm submodule update --init --recursive`
