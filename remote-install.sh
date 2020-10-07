dfa1d8e55f5eb5a818cef9e0ba20d1f919e749d5
Mateusz Duda <the_pro@wp.pl>
2020-10-07 23:06:35 +0200

    typ

--- remote-install.sh
+++ remote-install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-function check_prerequisite {
-    if ! command -v $1 &> /dev/null
-    then
+function check_prerequisite() {
+    if ! command -v $1 &>/dev/null; then
         echo "$1 is required"
         exit
     fi
 }
 
 # check prerequisites
-for i in git zsh curl 
-do
+for i in git zsh curl; do
     check_prerequisite $i
 done
 
 # check if yadm is installed
-if ! command -v yadm &> /dev/null
-then
+if ! command -v yadm &>/dev/null; then
     curl -fLo .yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x .yadm
 fi
 
